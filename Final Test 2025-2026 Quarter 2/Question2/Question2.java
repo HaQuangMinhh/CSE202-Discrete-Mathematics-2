9bddf639718eb9cf02d8d701d26ac132a5512280
HaQuangMinhh <155843525+HaQuangMinhh@users.noreply.github.com>
2025-04-04 23:18:12 +0700

    Add files via upload

---
+++ Final Test 2025-2026 Quarter 2/Question2/Question2.java
@@ -0,0 +1,216 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Queue;
+
+public class Question2 {
+
+    static StringBuilder sb = new StringBuilder();
+    static InputReader rd ;
+    public static void main(String[] args) throws IOException {
+        rd = new InputReader(System.in);
+
+        int n = rd.nextInt(); 
+        int m = rd.nextInt(); 
+
+        Vertex[] vertices = new Vertex[n]; 
+
+        for ( int i = 0 ; i < n ; i++ ) {
+            vertices[i] = new Vertex(i);
+        }
+
+        for ( int i = 1 ; i < n  ; i++ ) {
+            vertices[i].percent = rd.nextDouble();
+        }
+
+        for ( int i = 0 ; i < n - 1 ; i++ ) {
+            int u = rd.nextInt(); 
+            int v = rd.nextInt(); 
+
+            vertices[u].addAdjecentVertices(vertices[v]);
+        }
+
+        vertices[0].percent = 100 ; 
+        vertices[0].budget = m ; 
+
+        bfs(vertices[0]);
+
+        for ( Vertex vertex : vertices ) {
+            sb.append( (int) Math.round(vertex.budget)).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+    
+    static void bfs ( Vertex vertex ) {
+
+        Queue<Vertex> q = new ArrayDeque<>();
+
+        vertex.visited = true ; 
+        q.add(vertex);
+
+        while ( ! q.isEmpty() ) {
+            Vertex vertex2 = q.poll();
+
+            for ( Vertex vertex3 : vertex2.adjecentVertices ) {
+
+                if ( ! vertex3.visited ) {
+                    vertex3.visited = true ; 
+                    vertex3.budget = vertex2.budget * vertex3.percent / 100 ; 
+
+                    q.add(vertex3);
+                }
+            }
+        }
+        
+    }
+
+    static class Vertex {
+        public int id ; 
+        public double percent ; 
+        public boolean visited ; 
+        public double budget ; 
+
+        public List<Vertex> adjecentVertices = new ArrayList<>(); 
+
+        public Vertex ( int id ) {
+            this.id = id ; 
+        }
+
+        public void addAdjecentVertices ( Vertex v ) {
+            adjecentVertices.add(v);
+        }
+    }
+
+    static class InputReader {
+        private byte[] inbuf = new byte[2 << 23];
+        public int lenbuf = 0, ptrbuf = 0;
+        public InputStream is;
+
+        public InputReader(InputStream stream) throws IOException {
+
+            inbuf = new byte[2 << 23];
+            lenbuf = 0;
+            ptrbuf = 0;
+            is = System.in;
+            lenbuf = is.read(inbuf);
+        }
+
+        public InputReader(FileInputStream stream) throws IOException {
+            inbuf = new byte[2 << 23];
+            lenbuf = 0;
+            ptrbuf = 0;
+            is = stream;
+            lenbuf = is.read(inbuf);
+        }
+
+        public boolean hasNext() throws IOException {
+            if (skip() >= 0) {
+                ptrbuf--;
+                return true;
+            }
+            return false;
+        }
+
+        public String nextLine() throws IOException {
+            int b = skip();
+            StringBuilder sb = new StringBuilder();
+            while (!isSpaceChar(b) && b != ' ') { // when nextLine, ()
+                sb.appendCodePoint(b);
+                b = readByte();
+            }
+            return sb.toString();
+        }
+
+        public String next() {
+            int b = skip();
+            StringBuilder sb = new StringBuilder();
+            while (!(isSpaceChar(b))) { // when nextLine, (isSpaceChar(b) && b
+                                        // != ' ')
+                sb.appendCodePoint(b);
+                b = readByte();
+            }
+            return sb.toString();
+        }
+
+        private int readByte() {
+            if (lenbuf == -1)
+                throw new InputMismatchException();
+            if (ptrbuf >= lenbuf) {
+                ptrbuf = 0;
+                try {
+                    lenbuf = is.read(inbuf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (lenbuf <= 0)
+                    return -1;
+            }
+            return inbuf[ptrbuf++];
+        }
+
+        private boolean isSpaceChar(int c) {
+            return !(c >= 33 && c <= 126);
+        }
+
+        private double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public Character nextChar() {
+            return skip() >= 0 ? (char) skip() : null;
+        }
+
+        private int skip() {
+            int b;
+            while ((b = readByte()) != -1 && isSpaceChar(b))
+                ;
+            return b;
+        }
+
+        public int nextInt() {
+            int num = 0, b;
+            boolean minus = false;
+            while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+                ;
+            if (b == '-') {
+                minus = true;
+                b = readByte();
+            }
+
+            while (true) {
+                if (b >= '0' && b <= '9') {
+                    num = num * 10 + (b - '0');
+                } else {
+                    return minus ? -num : num;
+                }
+                b = readByte();
+            }
+        }
+
+        public long nextLong() {
+            long num = 0;
+            int b;
+            boolean minus = false;
+            while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+                ;
+            if (b == '-') {
+                minus = true;
+                b = readByte();
+            }
+
+            while (true) {
+                if (b >= '0' && b <= '9') {
+                    num = num * 10 + (b - '0');
+                } else {
+                    return minus ? -num : num;
+                }
+                b = readByte();
+            }
+        }
+    }
+}
